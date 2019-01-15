16ee265d286a3cd6d913a1e6458ac327e1babe96
Maria Palma <mapalma@mapalma-macbookpro.roam.corp.google.com>
2019-01-14 20:53:46 -0600

    Un cronometro muy cool y dificil

--- starter-code/javascript/chronometer.js
+++ starter-code/javascript/chronometer.js
@@ -1,39 +1,51 @@
 // Constructor
-// function Chronometer() {
+function Chronometer() {
+    this.currentTime = 0
+    this.intervalID = undefined
+    this.minutos = 0
+    this.segundos = 0
+}
 
-// }
+Chronometer.prototype.startClick = function () {
+    this.intervalID = setInterval(function(){
+        this.currentTime ++
+        this.setTime()
+    }.bind(this), 1000)
+};
 
-// Chronometer.prototype.startClick = function () {
+Chronometer.prototype.setMinutes = function () {
+    return this.minutes = Math.floor(this.currentTime / 60)
+};
 
-// };
+Chronometer.prototype.setSeconds = function () {
+    return this.seconds = this.currentTime % 60
+};
 
-// Chronometer.prototype.setMinutes = function () {
-  
-// };
+Chronometer.prototype.twoDigitsNumber = function (val) {
+  if(val<10){
+      return '0'+ val
+  }else{
+      return ''+val
+  }
+};
 
-// Chronometer.prototype.setSeconds = function () {
-  
-// };
+Chronometer.prototype.setTime = function () {
+    this.seconds = this.twoDigitsNumber(this.setSeconds())
+    this.minutes = this.twoDigitsNumber(this.setMinutes())
+    console.log(this.minutes + ' : ' + this.seconds)
+};
 
-// Chronometer.prototype.twoDigitsNumber = function () {
-  
-// };
+//Chronometer.prototype.setMilliseconds = function () {
+   
+//};
 
-// Chronometer.prototype.setTime = function () {
+Chronometer.prototype.stopClick = function () {
+    clearInterval(this.intervalID)
+};
 
-// };
-
-// Chronometer.prototype.setMilliseconds = function () {
-
-// };
-
-// Chronometer.prototype.stopClick = function () {
-  
-// };
-
-// Chronometer.prototype.resetClick = function () {
-
-// };
+Chronometer.prototype.resetClick = function () {
+    this.currentTime = 0
+};
 
 // Chronometer.prototype.splitClick = function () {
 
